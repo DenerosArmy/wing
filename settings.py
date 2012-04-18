e1ab2e81049afde8ceecbe3cf6136dd91299a136
Vaishaal <vaishaal@berkeley.edu>
2012-04-17 17:08:53 -0700

    Initial Commit

--- settings.py
+++ settings.py
@@ -11,7 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+	
         'NAME': 'main.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
